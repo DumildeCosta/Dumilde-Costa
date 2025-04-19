0801408e04e125c175d3b154146e4fb0774ad0b7
Dumilde Costa <dumildecosta10@gmail.com>
2025-04-19 22:04:40 +0100

    Modificações

--- scrypt.js
+++ scrypt.js
@@ -2,3 +2,5 @@ document.addEventListener("DOMContentLoaded", () =>
 	{
 		console.log("Página carregada com sucesso!");
 	});
+
+console.log("Script.carregando");
---
+++ test/sript.js
@@ -0,0 +1,3 @@
+// script.js
+console.log("Script carregado!");
+// Você pode adicionar interações ou funcionalidades futuras aqui.
